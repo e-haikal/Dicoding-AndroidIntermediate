854635ddca85c4fee32a1eb046c10398deb1aa0b
haikal <asmanhaikal@gmail.com>
2024-11-11 12:32:05 +0700

    feat(mw): completing 'latihan menampilkan widget di homepage'

--- MyWidgets/app/build.gradle.kts
+++ MyWidgets/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.siaptekno.mywidgets"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.siaptekno.mywidgets"
@@ -32,6 +32,9 @@ android {
     }
     kotlinOptions {
         jvmTarget = "1.8"
+    }
+    buildFeatures {
+        viewBinding = true
     }
 }
 
