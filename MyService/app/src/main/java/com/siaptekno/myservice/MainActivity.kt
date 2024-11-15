5ba25be9fd892157027fa1b86e2d61bc3597a15b
haikal <asmanhaikal@gmail.com>
2024-11-15 16:31:07 +0700

    feat: completing 'latihan service (bg service)'

--- MyService/app/src/main/java/com/siaptekno/myservice/MainActivity.kt
+++ MyService/app/src/main/java/com/siaptekno/myservice/MainActivity.kt
@@ -1,20 +1,31 @@
 package com.siaptekno.myservice
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.siaptekno.myservice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Initialize the service intent
+        val serviceIntent = Intent(this, MyBackgroundService::class.java)
+
+        // Set up button click listeners to start and stop the service
+        binding.btnStartBackgroundService.setOnClickListener {
+            startService(serviceIntent)
+        }
+        binding.btnStopBackgroundService.setOnClickListener {
+            stopService(serviceIntent)
         }
     }
 }
